a15c987f5e4284a2bb85af21f7939ca211623da0
Elisha Schwarz <eschwarz0@gmail.com>
2021-10-08 15:00:12 -0400

    attempt to work from lapotp

--- MW5_MM_Core/NexusAPI.cs
+++ MW5_MM_Core/NexusAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,6 @@ namespace Nexus_Core
     }
     public class NexusAPI : INexusApi
     {
-        
+        private IConfiguration _config;
     }
 }
