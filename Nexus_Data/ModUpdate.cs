1166e2b50d1938a2af02674fd127a295698bde14
pnkfld7892 <eschwarz0@gmail.com>
2021-10-08 13:56:10 -0400

    add mod update to data library

---
+++ Nexus_Data/ModUpdate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nexus_Data
+{
+    public class ModUpdate
+    {
+        public int mod_id { get; set; }
+        public int latest_file_update { get; set; }
+        public int latest_mod_activity { get; set; }
+    }
+}
