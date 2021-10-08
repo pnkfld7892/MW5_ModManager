872a6c4b75584f314f862d174f6a78a1641b3167
pnkfld7892 <eschwarz0@gmail.com>
2021-10-08 14:05:48 -0400

    add nexus api in to di

--- MW5_MM_UI/Form1.cs
+++ MW5_MM_UI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nexus_Core;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,11 @@ namespace MW5_MM_UI
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private INexusApi _nexusApi;
+        public Form1(INexusApi nexusApi)
         {
             InitializeComponent();
+            _nexusApi = nexusApi;
         }
 
     }
